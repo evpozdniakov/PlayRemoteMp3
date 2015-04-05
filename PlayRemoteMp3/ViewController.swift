68c98bda6c0c9ead8b1c209aff9e496ff37c1d10
Evgeniy Pozdnyakov <evp@Evgeniys-MacBook-Pro.local>
2015-04-04 23:31:00 -0400

    Some refactoring.

--- PlayRemoteMp3/ViewController.swift
+++ PlayRemoteMp3/ViewController.swift
@@ -14,24 +14,27 @@ class ViewController: UIViewController {
         case Unknown
         case Playing
         case Paused
+        case SeekingWhilePaused
+        case SeekingWhilePlaying
     }
     
     // #MARK: - ivars
 
     var player = AVPlayer()
-    var playerItem = AVPlayerItem()
     var pausedAt: CMTime?
     var trackDuration: CMTime?
     var playerStatus: MyAVPlayerStatus = .Unknown
-    var timer: NSTimer?
-    var isSeeking = false
+    var refreshSliderTimer: NSTimer?
+    let refreshSliderEvery = 0.1
+    let playerKeysToObserve = ["rate", "status"]
 
-    @IBOutlet weak var remoteMp3PlayBtn: UIButton!
-    @IBOutlet weak var remoteMp3PauseBtn: UIButton!
-    @IBOutlet weak var remoteMp3ResumeBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    @IBOutlet weak var resumeBtn: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
-    @IBOutlet weak var seekSlider: UISlider!
-    @IBOutlet weak var trackPlayingTime: UILabel!
+    @IBOutlet weak var currentTimeSlider: UISlider!
+    @IBOutlet weak var currentTimeLbl: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // #MARK: - UIViewController methods
     
@@ -45,108 +48,138 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    deinit {
+        if playerStatus == .Playing {
+            player.pause()
+        }
+
+        stopObservingPlayerKeyPaths()
+    }
     
     // #MARK: - event listeners
 
-    @IBAction func remoteMp3PlayBtnClicked(sender: AnyObject) {
-        remoteMp3Play()
+    @IBAction func playBtnClicked(sender: AnyObject) {
+        startPlayback()
     }
 
-    @IBAction func remoteMp3PauseBtnClicked(sender: AnyObject) {
-        remoteMp3Pause()
+    @IBAction func pauseBtnClicked(sender: AnyObject) {
+        pausePlayback()
     }
 
-    @IBAction func remoteMp3ResumeBtnClicked(sender: AnyObject) {
-        remoteMp3Resume()
+    @IBAction func resumeBtnClicked(sender: AnyObject) {
+        resumePlayback()
     }
 
-    @IBAction func remoteMp3VolumeChanged(sender: AnyObject) {
-        let slider = sender as UISlider
-        remoteMp3SetVolumeTo(slider.value)
+    @IBAction func remoteMp3VolumeChanged(sender: UISlider) {
+        playbackSetVolumeTo(sender.value)
     }
 
     @IBAction func touchDown(sender: UISlider) {
-//        remoteMp3Pause()
-        isSeeking = true
+       playbackStartSeeking()
     }
 
     @IBAction func touchUpInside(sender: UISlider) {
-        if let duration = trackDuration {
-            let position = Float(seekSlider.value)
-            let value = Float(duration.value) * position
-            let seekTo = CMTimeMake(Int64(value), duration.timescale)
-            println("seek to \(seekTo)")
-            player.seekToTime(seekTo)
-        }
-        isSeeking = false
+        playbackCompleteSeeking()
     }
 
     @IBAction func touchUpOutside(sender: UISlider) {
-        isSeeking = false
+        playbackCompleteSeeking()
     }
 
-    // #MARK: - helpers
+    func startObservingPlayerKeyPaths() {
+        for keyPath in playerKeysToObserve {
+            player.addObserver(self, forKeyPath: keyPath, options: .New, context: nil)
+        }
+    }
 
-    func remoteMp3Play() {
-        //let url = "http://mds.kallisto.ru/Kir_Bulychev_-_Zvjozdy_zovut.mp3"
-        let url = "http://mds.kallisto.ru/roygbiv/records/mp3/Leonid_Kaganov_-_Choza_griby.mp3"
-        playerItem = AVPlayerItem( URL:NSURL( string:url ) )
+    func stopObservingPlayerKeyPaths() {
+        for keyPath in playerKeysToObserve {
+            player.removeObserver(self, forKeyPath: keyPath)
+        }
+    }
+
+    // #MARK: - playback
+
+    func startPlayback() {
+        let url = "http://goo.gl/oxlCUq"
+        // let url = "http://mds.kallisto.ru/Kir_Bulychev_-_Zvjozdy_zovut.mp3"
+        // let url = "http://mds.kallisto.ru/roygbiv/records/mp3/Leonid_Kaganov_-_Choza_griby.mp3"
+        let playerItem = AVPlayerItem( URL:NSURL( string:url ) )
         player = AVPlayer(playerItem:playerItem)
-        player.addObserver(self, forKeyPath: "rate", options: .New, context: nil)
-        player.addObserver(self, forKeyPath: "status", options: .New, context: nil)
-        playerItem.addObserver(self, forKeyPath: "timedMetadata", options: .New, context: nil)
+        startObservingPlayerKeyPaths()
         player.rate = 1.0;
         player.play()
     }
     
-    func remoteMp3Pause() {
+    func pausePlayback() {
         pausedAt = player.currentTime()
         player.pause()
     }
     
-    func remoteMp3Resume() {
+    func resumePlayback() {
         player.play()
         if let pausedAt = pausedAt {
             player.seekToTime(pausedAt)
         }
     }
     
-    func remoteMp3SetVolumeTo(value: Float) {
+    func playbackSetVolumeTo(value: Float) {
         player.volume = value
     }
+
+    func playbackStartSeeking() {
+        playerStatus = playerStatus == .Playing ? .SeekingWhilePlaying : .SeekingWhilePaused
+    }
+
+    func playbackCompleteSeeking() {
+        if let duration = trackDuration {
+            let position = Float(currentTimeSlider.value)
+            let value = Float(duration.value) * position
+            let seekTo = CMTimeMake(Int64(value), duration.timescale)
+
+            if playerStatus == .SeekingWhilePaused {
+                pausedAt = seekTo
+                playerStatus = .Paused
+            }
+            else {
+                player.seekToTime(seekTo)
+                playerStatus = .Playing
+            }
+        }
+    }
+
+    // #MARK: - redrawnings
     
     override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
         if let object = object as? AVPlayer {
             if object == player {
                 if keyPath == "rate" {
-                    println("rate changed: \(player.rate)")
+                    if player.status != .ReadyToPlay {
+                        println("--- not ready yet: \(player.status)")
+                        return;
+                    }
                     if player.rate > 0 {
+                        println("--- rate > 0")
                         playerStatus = .Playing
-                        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("configureRemoteMp3Controls"), userInfo: nil, repeats: true)
+                        refreshSliderTimer = NSTimer.scheduledTimerWithTimeInterval(refreshSliderEvery, target: self, selector: Selector("configureRemoteMp3Controls"), userInfo: nil, repeats: true)
                     }
                     else {
                         playerStatus = .Paused
-                        timer?.invalidate()
+                        refreshSliderTimer?.invalidate()
                     }
                     configureRemoteMp3Controls()
                 }
                 else if keyPath == "status" {
-                    println("status changed: \(player.status)")
                     if player.status == .Unknown {
                         playerStatus = .Unknown
-                        timer?.invalidate()
+                        refreshSliderTimer?.invalidate()
                         configureRemoteMp3Controls()
                     }
                     else if player.status == .ReadyToPlay {
+                        println("---ready to play")
                         trackDuration = player.currentItem.asset.duration
                     }
-                }
-            }
-        }
-        else if let object = object as? AVPlayerItem {
-            if object == playerItem {
-                if keyPath == "timedMetadata" {
-                    println("timedMetadata: \(player.currentItem.timedMetadata)")
                 }
             }
         }
@@ -158,52 +191,39 @@ class ViewController: UIViewController {
     func configureRemoteMp3Controls() {
         switch playerStatus {
         case .Unknown:
-            remoteMp3PlayBtn.enabled = true
-            remoteMp3PauseBtn.enabled = false
-            remoteMp3ResumeBtn.enabled = false
+            playBtn.enabled = true
+            pauseBtn.enabled = false
+            resumeBtn.enabled = false
             volumeSlider.enabled = false
-            seekSlider.enabled = false
+            currentTimeSlider.enabled = false
+            currentTimeLbl.hidden = true
         case .Playing:
-            remoteMp3PlayBtn.enabled = false
-            remoteMp3PauseBtn.enabled = true
-            remoteMp3ResumeBtn.enabled = false
+            playBtn.enabled = false
+            pauseBtn.enabled = true
+            resumeBtn.enabled = false
             volumeSlider.enabled = true
-            seekSlider.enabled = true
-            if !isSeeking {
-                if let duration = trackDuration {
-                    let currentTime = player.currentItem.currentTime()
-                    let position = CMTimeGetSeconds(currentTime) / CMTimeGetSeconds(duration)
-                    seekSlider.value = Float(position)
-                    
-                    let currentTimeSeconds = lroundf(Float(CMTimeGetSeconds(currentTime)))
-                    let hours = currentTimeSeconds / 3600
-                    println("hours: \(hours)")
-                    let minutes = currentTimeSeconds / 60 % 60
-                    println("minutes: \(minutes)")
-                    let seconds = currentTimeSeconds % 60
-                    println("seconds: \(seconds)")
-                    trackPlayingTime.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-                }
+            currentTimeSlider.enabled = true
+            if let duration = trackDuration {
+                let currentTime = player.currentItem.currentTime()
+                let position = CMTimeGetSeconds(currentTime) / CMTimeGetSeconds(duration)
+                currentTimeSlider.value = Float(position)
+                
+                let currentTimeSeconds = lroundf(Float(CMTimeGetSeconds(currentTime)))
+                let hours = currentTimeSeconds / 3600
+                let minutes = currentTimeSeconds / 60 % 60
+                let seconds = currentTimeSeconds % 60
+                currentTimeLbl.hidden = false
+                currentTimeLbl.text = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
             }
         case .Paused:
-            remoteMp3PlayBtn.enabled = false
-            remoteMp3PauseBtn.enabled = false
-            remoteMp3ResumeBtn.enabled = true
+            playBtn.enabled = false
+            pauseBtn.enabled = false
+            resumeBtn.enabled = true
             volumeSlider.enabled = true
-            seekSlider.enabled = true
+            currentTimeSlider.enabled = true
         default:
             break        
         }
-    }
-
-    deinit {
-        if playerStatus == .Playing {
-            player.pause()
-        }
-        
-        player.removeObserver(self, forKeyPath: "status")
-        player.removeObserver(self, forKeyPath: "rate")
-        playerItem.removeObserver(self, forKeyPath: "timedMetadata")
     }
 }
 
